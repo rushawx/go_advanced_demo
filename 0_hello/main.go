415786a5ab4e66406b2472b5e65aa4aecbb231be
Anton <rushawx@gmail.com>
2024-12-24 20:30:37 +0300

    demo

--- 0_hello/main.go
+++ 0_hello/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := http.NewServeMux()
+
+	NewHelloHandler(router)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
 }

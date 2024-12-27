b52d1f3bd8a8dea884328bb4c5d6e877134b43f1
Anton <rushawx@gmail.com>
2024-12-27 13:16:26 +0300

    demo

---
+++ 0-hello/internal/link/model.go
@@ -0,0 +1,31 @@
+package link
+
+import (
+	"math"
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func NewLink(url string) *Link {
+	return &Link{
+		Url:  url,
+		Hash: RandStringRunes(int(math.Log(float64(len(url))))),
+	}
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}

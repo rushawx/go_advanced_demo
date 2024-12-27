2ed2b174d1aab47dfd843cb07aa8b4c3e91e8315
Anton <rushawx@gmail.com>
2024-12-27 22:28:11 +0300

    hw

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"4-order-api/configs"
 	"4-order-api/internal/product"
 	"4-order-api/pkg/db"
+	"4-order-api/pkg/middleware"
 	"fmt"
 	"net/http"
 )
@@ -20,9 +21,13 @@ func main() {
 
 	product.NewProductHandler(router, product.ProductHandlerDeps{ProductRepository: productRepository})
 
+	stack := middleware.Chain(
+		middleware.Logging,
+	)
+
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: stack(router),
 	}
 
 	fmt.Println("Server is listening on port 8080")
--- 4-order-api/go.mod
+++ 4-order-api/go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/lib/pq v1.10.9
+	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
